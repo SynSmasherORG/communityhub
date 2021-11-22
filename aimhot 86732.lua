48f2be282647e5fdc62f1e2303fa330217c6c8ca
SynSmasherCORP <94846228+SynSmasherCORP@users.noreply.github.com>
2021-11-22 15:34:15 +0200

    Add files via upload

---
+++ aimhot 86732.lua
@@ -0,0 +1,7 @@
+--[[
+    AimHot v8, made by Herrtt#3868
+    for y'all clanners out there
+]]
+pcall(function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Herrtt/AimHot-v8/master/Main.lua", true))()
+end)
